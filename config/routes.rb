c92690ddc47dc7c840d0153f708bebc7de978536
Jesus Martinez <jesus.alberto.vk@gmail.com>
2021-04-19 10:21:45 -0500

    Block 5

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@ Rails.application.routes.draw do
   scope '(:locale)', locale: /es|en/ do 
     get 'home/grettings'
     namespace :v1, defaults: { format: 'json' } do
-      resources :users, only: %i[create]
+      resources :users, only: %i[create] do
+        post 'login', on: :collection 
+      end
     end
   end
 end
--- spec/routing/user_spec.rb
+++ spec/routing/user_spec.rb
@@ -8,4 +8,11 @@ describe 'Rutas de resources user' do
             action: 'create'
         )
     end
+    it 'Login del usuario' do
+        expect(post: '/v1/users/login').to route_to(
+            format: 'json',
+            controller: 'v1/users',
+            action: 'login'
+        )
+    end
 end
