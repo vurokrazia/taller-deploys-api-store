24dbd95ea4e750e5bd711bd341040d3a90dfdf42
Jesus <jesus.alberto.vk@gmail.com>
2022-07-06 17:35:42 -0500

    Update configuration to I18n

--- config/application.rb
+++ config/application.rb
@@ -23,8 +23,8 @@ module StoreApi
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
-    I18n.default_locale = :es
     I18n.available_locales = %i[es en]
+    I18n.default_locale = :es
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
